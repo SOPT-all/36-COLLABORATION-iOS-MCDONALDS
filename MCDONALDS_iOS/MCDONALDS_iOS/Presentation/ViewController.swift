9c26e2c66b5ec05a3bbbc5d320a62a5cbd27dd1e
Won <andrew112517@naver.com>
2025-05-09 16:05:40 +0900

    🎉 Project setting

---
+++ MCDONALDS_iOS/MCDONALDS_iOS/Presentation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MCDONALDS_iOS
+//
+//  Created by 김승원 on 5/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
