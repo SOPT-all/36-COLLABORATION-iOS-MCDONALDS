42a711194e8b8b539a6af527fe73976008f3e4e4
Won <andrew112517@naver.com>
2025-05-12 02:01:46 +0900

    ✨ UILabel+ Extension
    Develop an addUnderline function

---
+++ MCDONALDS_iOS/MCDONALDS_iOS/Global/Extensions/UILabel+.swift
@@ -0,0 +1,22 @@
+//
+//  UILabel+.swift
+//  MCDONALDS_iOS
+//
+//  Created by 김승원 on 5/12/25.
+//
+
+import UIKit
+
+extension UILabel {
+    /// UILabel에 밑줄을 추가하는 함수입니다
+    func addUnderline() {
+        guard let labelText = self.text else { return }
+        let attributedString = NSMutableAttributedString(string: labelText)
+        attributedString.addAttribute(
+            .underlineStyle,
+            value: NSUnderlineStyle.single.rawValue,
+            range: NSRange(location: 0, length: attributedString.length)
+        )
+        self.attributedText = attributedString
+    }
+}
