e64a247695355d900454ae85c8911da1cccbfc85
Won <andrew112517@naver.com>
2025-05-14 02:22:13 +0900

    ✨ CustomNavigationBar

--- MCDONALDS_iOS/MCDONALDS_iOS/Global/Base/BaseViewController.swift
+++ MCDONALDS_iOS/MCDONALDS_iOS/Global/Base/BaseViewController.swift
@@ -26,3 +26,91 @@ class BaseViewController: UIViewController {
     func setDelegate() {}
 }
 
+// MARK: - Custom NavigationBar
+
+extension BaseViewController {
+    
+    /// Mcdonalds 커스텀 네비게이션 바 세팅 함수입니다.
+    /// - Parameter type: 네비게이션 바 타입 (McdonalsNavigationBarType)
+    func setNavigationBar(type: McdonalsNavigationBarType) {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithDefaultBackground()
+        appearance.backgroundColor = .staticWhite
+        appearance.shadowColor = nil
+        
+        title = type.title
+        let titleFont = UIFont.pretendard(.headBold16)
+        let titleAttributes: [NSAttributedString.Key: Any] = [
+            .font: titleFont,
+            .foregroundColor: UIColor.staticBlack
+        ]
+        appearance.titleTextAttributes = titleAttributes
+        
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        
+        let leftItem = UIBarButtonItem(
+            image: type.leftBarButtonItemImage,
+            style: .done,
+            target: self,
+            action: #selector(leftBarButtonItemDidTap)
+        )
+        leftItem.tintColor = .grayScale800
+        navigationItem.leftBarButtonItem = leftItem
+        
+        if let rightItem = type.rightBarButtonItem {
+            navigationItem.rightBarButtonItem = rightItem
+        }
+    }
+    
+    @objc
+    private func leftBarButtonItemDidTap() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    enum McdonalsNavigationBarType {
+        case findStore
+        case menuList
+        case order
+        case orderList
+        
+        var title: String {
+            switch self {
+            case .findStore:
+                return "매장찾기"
+            case .menuList:
+                return "버거 & 세트"
+            case .order:
+                return "주문하기"
+            case .orderList:
+                return "주문내역"
+            }
+        }
+        
+        var leftBarButtonItemImage: UIImage {
+            switch self {
+            case .findStore, .orderList:
+                return .iconClose
+            case .menuList, .order:
+                return .iconBackarrow
+            }
+        }
+        
+        var rightBarButtonItem: UIBarButtonItem? {
+            switch self {
+            case .findStore:
+                let item = UIBarButtonItem(
+                    image: .iconSearch,
+                    style: .plain,
+                    target: nil,
+                    action: nil
+                )
+                item.tintColor = .grayScale800
+                return item
+            default:
+                return nil
+            }
+        }
+    }
+}
