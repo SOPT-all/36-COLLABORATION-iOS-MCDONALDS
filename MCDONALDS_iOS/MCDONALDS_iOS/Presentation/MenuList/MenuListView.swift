8c450cd405e1a70c28f6b599768fee24dc2a2bf0
Won <andrew112517@naver.com>
2025-05-20 01:48:23 +0900

    ✨ Active MenuList FilterButton

--- MCDONALDS_iOS/MCDONALDS_iOS/Presentation/MenuList/MenuListView.swift
+++ MCDONALDS_iOS/MCDONALDS_iOS/Presentation/MenuList/MenuListView.swift
@@ -14,17 +14,93 @@ final class MenuListView: BaseView {
     
     // MARK: - UI Properties
     
+    let newMenuButton = FilterButton("신메뉴", isSelected: true)
+    
+    let recommendButton = FilterButton("추천메뉴")
+    
+    let crispyButton = FilterButton("맥크리스피")
+    
+    let bulgogiButton = FilterButton("불고기")
+    
+    let cheeseButton = FilterButton("치즈")
+    
+    private let filterScrollView = UIScrollView()
+    
+    private let filterContainerView = UIView()
+    
     // MARK: - UI Setting
     
     override func setStyle() {
+        self.do {
+            $0.backgroundColor = .staticWhite
+        }
         
+        filterScrollView.do {
+            $0.showsHorizontalScrollIndicator = false
+            $0.showsVerticalScrollIndicator = false
+        }
     }
     
     override func setUI() {
+        addSubview(filterScrollView)
         
+        filterScrollView.addSubview(filterContainerView)
+        
+        filterContainerView.addSubviews(
+            newMenuButton,
+            recommendButton,
+            crispyButton,
+            bulgogiButton,
+            cheeseButton
+        )
     }
     
     override func setLayout() {
+        filterScrollView.snp.makeConstraints {
+            $0.top.equalTo(safeAreaLayoutGuide.snp.top)
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(46)
+        }
         
+        filterContainerView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+            $0.height.equalToSuperview()
+            $0.trailing.equalTo(cheeseButton.snp.trailing).offset(19)
+        }
+        
+        newMenuButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().inset(19)
+            $0.height.equalTo(30)
+            $0.width.equalTo(61)
+        }
+        
+        recommendButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalTo(newMenuButton.snp.trailing).offset(8)
+            $0.height.equalTo(30)
+            $0.width.equalTo(71)
+        }
+        
+        crispyButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalTo(recommendButton.snp.trailing).offset(8)
+            $0.height.equalTo(30)
+            $0.width.equalTo(80)
+        }
+        
+        bulgogiButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalTo(crispyButton.snp.trailing).offset(8)
+            $0.height.equalTo(30)
+            $0.width.equalTo(61)
+        }
+        
+        cheeseButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalTo(bulgogiButton.snp.trailing).offset(8)
+            $0.height.equalTo(30)
+            $0.width.equalTo(52)
+        }
     }
 }
--- MCDONALDS_iOS/MCDONALDS_iOS/Presentation/MenuList/MenuListViewController.swift
+++ MCDONALDS_iOS/MCDONALDS_iOS/Presentation/MenuList/MenuListViewController.swift
@@ -13,15 +13,79 @@ final class MenuListViewController: BaseViewController {
     
     private let rootView = MenuListView()
     
+    private var filterButtons: [UIButton] = []
+    
     // MARK: - Life Cycle
     
     override func loadView() {
         view = rootView
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setFilterButtons()
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         setNavigationBar(type: .menuList)
+    }
+    
+    override func setAction() {
+        rootView.newMenuButton.addTarget(
+            self,
+            action: #selector(filterButtonDidTap),
+            for: .touchUpInside
+        )
+        
+        rootView.recommendButton.addTarget(
+            self,
+            action: #selector(filterButtonDidTap),
+            for: .touchUpInside
+        )
+        
+        rootView.crispyButton.addTarget(
+            self,
+            action: #selector(filterButtonDidTap),
+            for: .touchUpInside
+        )
+        
+        rootView.bulgogiButton.addTarget(
+            self,
+            action: #selector(filterButtonDidTap),
+            for: .touchUpInside
+        )
+        
+        rootView.cheeseButton.addTarget(
+            self,
+            action: #selector(filterButtonDidTap),
+            for: .touchUpInside
+        )
+    }
+}
+
+// MARK: - Functions
+
+extension MenuListViewController {
+    private func setFilterButtons() {
+        [
+            rootView.newMenuButton,
+            rootView.recommendButton,
+            rootView.crispyButton,
+            rootView.bulgogiButton,
+            rootView.cheeseButton
+        ].forEach { button in
+            filterButtons.append(button)
+        }
+    }
+    
+    @objc
+    private func filterButtonDidTap(_ sender: UIButton) {
+        for button in filterButtons {
+            let isSelected = (button == sender)
+            button.isSelected = isSelected
+        }
     }
 }
